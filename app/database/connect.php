2b17bc55fcad653b1dd1e825036f16d88f269682
Mamleev Muhammad <muhammad.mamleev.1998@gmail.com>
2023-11-05 16:12:04 +0500

    Добавил общие запросы на SELECT, INSERT, UPDATE и DELETE

--- app/database/connect.php
+++ app/database/connect.php
@@ -6,7 +6,8 @@ $db_name = 'discoverybashkiria';
 $db_user = 'root';
 $db_pass = 'mysql';
 $charset = 'utf8';
-$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
 
 try{
     $pdo = new PDO(
---
+++ app/database/db.php
@@ -0,0 +1,138 @@
+<?php
+
+require ('connect.php');
+
+function tt($value){
+    echo '<pre>';
+    print_r($value);
+    echo '<pre>';
+}
+
+// Проверка выполнения запроса
+function dbCheckError($query){
+    $errInfo = $query->errorInfo();
+    if ($errInfo[0] !== PDO::ERR_NONE){
+        echo $errInfo[2];
+        exit();
+    }
+    return true;
+}
+
+// Запрос на получение даннных из одной таблицы
+function selectAll($table, $params = []){
+    global $pdo;
+
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'" . $value . "'";
+            }
+
+            if ($i === 0){
+                $sql = $sql . " WHERE $key = $value";
+            }else{
+                $sql = $sql . " AND $key = $value";
+            }
+            $i++;
+        }
+    }
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+
+    dbCheckError($query);
+
+    return $query->fetchAll();
+}
+
+// Запрос на получение одной строки из таблицы
+function selectOne($table, $params = []){
+    global $pdo;
+
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'" . $value . "'";
+            }
+
+            if ($i === 0){
+                $sql = $sql . " WHERE $key = $value";
+            }else{
+                $sql = $sql . " AND $key = $value";
+            }
+            $i++;
+        }
+    }
+
+    // $sql = $sql . " LIMIT 1 ";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+
+    dbCheckError($query);
+
+    return $query->fetch();
+}
+
+// Запись в таблицу БД
+function insert($table, $params){
+    global $pdo;
+    $i = 0;
+    $col = '';
+    $mask = '';
+    foreach ($params as $key => $value){
+        if ($i === 0){
+            $col = $col . $key;
+            $mask = $mask . "'" . "$value" . "'";
+        }else{
+            $col = $col . ", $key";
+            $mask = $mask . ", '" . "$value" . "'";
+        }
+        $i++;
+    }
+
+    $sql = "INSERT INTO $table ($col) VALUES ($mask)";
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+
+    dbCheckError($query);
+}
+
+// Обновление строки в таблице
+function update($table, $user_id, $params){
+    global $pdo;
+    $i = 0;
+    $str = '';
+    foreach ($params as $key => $value){
+        if ($i === 0){
+            $str = $str . $key . " = '" . $value . "'";
+        }else{
+            $str = $str . ", $key" . " = '" . $value . "'";
+        }
+        $i++;
+    }
+
+    $sql = "UPDATE $table SET $str WHERE user_id = $user_id";
+
+    $query = $pdo->prepare($sql);
+    $query->execute($params);
+
+    dbCheckError($query);
+}
+
+// Удаление строки в таблице
+function delete($table, $user_id){
+    global $pdo;
+
+    $sql = "DELETE FROM $table WHERE user_id = $user_id";
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+
+    dbCheckError($query);
+}
